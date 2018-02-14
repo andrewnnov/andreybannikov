f07e8ceeea081b5d54a3803ac822bf27b8b6ff1c
andreybannikov <andrewnnov@yandex.ru>
2018-02-14 12:24:14 +0300

    5.0. Заполнить массив степенями чисел. [#33488]

---
+++ chapter_001/src/main/java/ru/job4j/array/Square.java
@@ -0,0 +1,22 @@
+package ru.job4j.array;
+/**
+ * @author andrewbannikov (andrewnnov@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Square {
+    /**
+     * array filling
+     * @param bound (bound>=1)
+     * @return full array
+     */
+    public int[] calculate(int bound){
+
+        int [] rsl = new int [bound];
+
+        for (int i = 1; i <= bound; i++){
+            rsl [i - 1] = i*i;
+        }
+        return rsl;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/SquareTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+/**
+ * @author andreybannikov
+ * @version $Id$
+ * @since 0.1
+ */
+public class SquareTest {
+
+    @Test
+    public void whenBoundThreeThenOneFourNine(){
+        Square square = new Square();
+        int[] resultArray = square.calculate(3);
+        int[] expectArray = {1, 4, 9};
+        assertThat(resultArray, is(expectArray));
+    }
+
+    @Test
+    public void whenBoundFiveThenOneFourNineSixteenTwFive(){
+        Square square = new Square();
+        int[] resultArray = square.calculate(5);
+        int[] expectArray = {1, 4, 9, 16, 25};
+        assertThat(resultArray, is(expectArray));
+    }
+
+
+}
