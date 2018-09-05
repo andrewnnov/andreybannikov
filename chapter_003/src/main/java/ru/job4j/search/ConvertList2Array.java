364b0275138352e746467bf445f65b6b2803f999
andreybannikov <andrewnnov@yandex.ru>
2018-09-05 20:17:55 +0300

    Преобразование List в двумерный массив

---
+++ chapter_003/src/main/java/ru/job4j/search/ConvertList2Array.java
@@ -0,0 +1,32 @@
+package ru.job4j.search;
+
+import java.util.List;
+
+public class ConvertList2Array {
+
+    int cells;
+
+    public int [] [] toArray(List<Integer> list, int rows){
+
+        if(list.size()% rows != 0){
+            cells = list.size()/rows + 1;
+        } else {
+            cells = list.size()/rows;
+        }
+        int [][] array = new int[rows][cells];
+
+        int listIndex = 0;
+
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j <cells; j++){
+               if(listIndex < list.size()){
+                   array[i][j] = list.get(listIndex++);
+               } else {
+                   array[i][j] = 0;
+               }
+            }
+        }
+        return array;
+    }
+
+}
