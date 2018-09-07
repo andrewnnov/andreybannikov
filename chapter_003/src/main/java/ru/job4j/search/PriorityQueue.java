56f1c6abb91f6c17c950033f362e373742710270
andreybannikov <andrewnnov@yandex.ru>
2018-09-07 12:18:11 +0300

    красивое решение добавление элемента в связный список

--- chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
+++ chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
@@ -12,23 +12,33 @@ public class PriorityQueue {
      * @param task задача
      */
     public void put(Task task) {
-        if (tasks.isEmpty()) {
-            tasks.add(task);
-        } else {
-            for (int i = 0; i < tasks.size(); ) {
-                if(task.getPriority() > tasks.get(tasks.size()-1).getPriority()){
-                    tasks.add(task);
-                    break;
-                }else
-                if (task.getPriority() > tasks.get(i).getPriority()) {
-                    i++;
-                } else {
-                    tasks.add(i, task);
-                    break;
-                }
+        int index = this.tasks.size();
+        for (int i = 0; i < tasks.size(); i++) {
+            if (task.getPriority() < tasks.get(i).getPriority()) {
+                index = i;
+                break;
             }
         }
+        tasks.add(index, task);
     }
+//    public void put(Task task) {
+//        if (tasks.isEmpty()) {
+//            tasks.add(task);
+//        } else {
+//            for (int i = 0; i < tasks.size(); ) {
+//                if(task.getPriority() > tasks.get(tasks.size()-1).getPriority()){
+//                    tasks.add(task);
+//                    break;
+//                }else
+//                if (task.getPriority() > tasks.get(i).getPriority()) {
+//                    i++;
+//                } else {
+//                    tasks.add(i, task);
+//                    break;
+//                }
+//            }
+//        }
+//    }
 
     public Task take() {
         return this.tasks.poll();
