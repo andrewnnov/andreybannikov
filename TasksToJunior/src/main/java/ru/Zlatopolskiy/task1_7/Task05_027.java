22e2497f8c93089735c9954c7ee3803e3b66f94e
andreybannikov <andrewnnov@yandex.ru>
2018-10-03 18:31:00 +0300

    Task05_27

--- TasksToJunior/src/main/java/ru/Zlatopolskiy/task1_7/Task05_027.java
+++ TasksToJunior/src/main/java/ru/Zlatopolskiy/task1_7/Task05_027.java
@@ -1,4 +1,84 @@
 package ru.Zlatopolskiy.task1_7;
 
+/*
+Найти:
+а) сумму всех целых чисел от 100 до 500;
+б) сумму всех целых чисел от a до 500 (значение a вводится с клавиатуры;
+a 500);
+в) сумму всех целых чисел от –10 до b (значение b вводится с клавиатуры;
+b –10);
+г) сумму всех целых чисел от a до b (значения a и b вводятся с клавиатуры;
+b a).
+ */
+
+import java.util.Scanner;
+
 public class Task05_027 {
+
+    public void getNumberFromKeyBoard() {
+        Scanner scanner = new Scanner(System.in);
+        int numberFromKeyBoardOne = scanner.nextInt();
+        int numberFromKeyBoardTwo = scanner.nextInt();
+
+
+    }
+
+
+    //a
+    public int sumRangeOfNumbers() {
+        int result = 0;
+        for (int i = 1; i <= 5 ; i++) {
+            result = result + i;
+        }
+
+        return result;
+    }
+
+    //б
+    public int sumRangeOfNumbersAto500(int a) {
+        int result = 0;
+        if(a <= 500) {
+            for (int i = a; i <= 500 ; i++) {
+                result = result + i;
+            }
+        } else {
+            result = -1111;
+        }
+        return result;
+
+    }
+
+    //в
+    public int sumRangeOfMinus10toB(int b) {
+        int result = 0;
+        if(b >= -10) {
+            for (int i = -10; i <= b ; i++) {
+                result = result + i;
+            }
+        } else {
+            result = -1111;
+        }
+        return result;
+    }
+
+    //г
+    public int sumRangeOfMinus10toB(int a, int b) {
+        int result = 0;
+        if(b >= a) {
+            for (int i = a; i <= b ; i++) {
+                result = result + i;
+            }
+        } else {
+            result = -1111;
+        }
+        return result;
+    }
+
+
+
+
+
+
+
+
 }
---
+++ TasksToJunior/src/test/java/ru/Zlatopolskiy/task1_7/Task05_27Test.java
@@ -0,0 +1,53 @@
+package ru.Zlatopolskiy.task1_7;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Task05_27Test {
+
+    //а
+    @Test
+    public void whenRange1_5ThenSum15() {
+        Task05_027 task = new Task05_027();
+
+        assertEquals(15,task.sumRangeOfNumbers());
+
+    }
+
+    //б
+    @Test
+    public void whenRange499_500ThenSum999() {
+        Task05_027 task = new Task05_027();
+        assertEquals(999,task.sumRangeOfNumbersAto500(499));
+    }
+
+    //в
+    @Test
+    public void whenRangeMinus10_BThenSumMinus19() {
+        Task05_027 task = new Task05_027();
+        assertEquals(-19,task.sumRangeOfMinus10toB(-9));
+        System.out.println(task.sumRangeOfMinus10toB(-9));
+    }
+
+    @Test
+    public void whenBOutRngeThenMinus1111() {
+        Task05_027 task = new Task05_027();
+        assertEquals(-1111,task.sumRangeOfMinus10toB(-11));
+        System.out.println(task.sumRangeOfMinus10toB(-11));
+    }
+    //г
+    @Test
+    public void whenRangeFromA1ToB2ThenSum3() {
+        Task05_027 task = new Task05_027();
+        assertEquals(3,task.sumRangeOfMinus10toB(1, 2));
+        System.out.println(task.sumRangeOfMinus10toB(1, 2));
+    }
+
+
+
+
+
+
+
+}
