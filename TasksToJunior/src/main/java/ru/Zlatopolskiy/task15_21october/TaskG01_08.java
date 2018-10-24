5ac5d04bb73ad687ecf576d0e51ef804de40bce8
andreybannikov <andrewnnov@yandex.ru>
2018-10-24 17:00:32 +0300

    промежуточный код для merge

---
+++ TasksToJunior/src/main/java/ru/Zlatopolskiy/task15_21october/TaskG01_08.java
@@ -0,0 +1,47 @@
+package ru.Zlatopolskiy.task15_21october;
+
+/*
+TaskG01_08: MergeSort - реализовать итеративное и рекурсивное решение
+ */
+
+public class TaskG01_08 {
+
+    public int[] mergeSort(int[] elements, int low, int high) {
+        if(low < high) {
+            int mid = (low + high)/2;
+            mergeSort(elements, low, mid);
+            mergeSort(elements, mid+1, high);
+            merge(elements, low, mid, high);
+        }
+        return elements;
+    }
+
+    private static void merge(int[] subset, int low, int mid, int high) {
+
+        int n = high - low + 1;
+        int[] temp = new int[n];
+
+        int i = low;
+        int j = mid+1;
+        int k = 0;
+
+        while (i <= mid || j<= high) {
+            if(i > mid)
+                temp[k++] = subset[j++];
+            else if(j > high)
+                temp[k++]= subset[i++];
+            else if(subset[i] < subset[j])
+                temp[k++] = subset[i++];
+            else
+                temp[k++] = subset[j++];
+        }
+
+        for (j = 0; j < n; j++) {
+            subset[low + j] = temp[j];
+
+        }
+    }
+
+
+
+}
